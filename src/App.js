6ffb4e02818b59aae6afb989ef3bfb82f5c59974
Akshya52 <aks575994@gmail.com>
2024-11-16 00:01:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from '../src/Components/Pages/Home'
+import { SnackbarProvider } from 'notistack';
+
+function App() {
+  return (
+    <SnackbarProvider >
+      <div>
+        <Home />
+      </div>
+    </SnackbarProvider>
+  );
+}
+
+export default App;
